08ca485ecb7f04895640ab7233592e65fee97077
C-A de Salaberry <cadesalaberry@yahoo.com>
2014-09-30 18:09:11 -0400

    Mod: replies to question correctly.

--- src/main/Graham.java
+++ src/main/Graham.java
@@ -1,8 +1,13 @@
 package main;
 
+import java.util.List;
+
 import main.tools.Timer;
 
 public class Graham {
+
+	final static int[] threads = { 2, 4, 8, 16, 32 };
+	final static int[] questions = { 1, 2, 3, 4 };
 
 	/**
 	 * @param args
@@ -10,6 +15,12 @@ public class Graham {
 	public static void main(String[] args) {
 
 		if (args.length < 3) {
+			//test();
+			//for (int j : questions) {
+				for (int i : threads) {
+					solveQuestion(4, 2222222, i);
+				}
+			//}
 			return;
 		}
 
@@ -22,22 +33,74 @@ public class Graham {
 		if (!validateArgs(q, n, p)) {
 			return;
 		}
+	}
 
-		//for (int j = 0; j <= 10; j++) {
-			
-			//System.out.println("Try #" + j + ":");
-			
-			for (int i = 1; i <= p; i++) {
-			
-				long t0 = System.currentTimeMillis();
-				
-				new UniquePointsFactory(i).randomize(n);
-				
-				Timer.printTiming(i, n, t0, System.currentTimeMillis());
-				
+	public static void solveQuestion(int q, int n, int p) {
 
-			}
-		//}
+		List<MyPoint> points;
+		long tq1, tq2, tq3, temp;
+
+		tq1 = System.currentTimeMillis();
+
+		points = new UniquePointsFactory(p).randomize(n);
+		
+		temp = System.currentTimeMillis();
+		Timer.printTiming(p, n, tq1, temp);
+		tq1 = temp - tq1;
+
+		if (q < 2)
+			return;
+
+		MTArrayList<MyPoint> mt = new MTArrayList<>(points);
+
+		tq2 = System.currentTimeMillis();
+		MyPoint min = mt.min();
+
+		temp = System.currentTimeMillis();
+		Timer.printTiming(p, n, tq2, temp);
+		tq2 = temp - tq2;
+		
+		if (q < 3)
+			return;
+
+		tq3 = System.currentTimeMillis();
+
+		//Comparator comp = new AngleComparator(min);
+		mt.sort();
+		
+		temp = System.currentTimeMillis();
+		Timer.printTiming(p, n, tq3, temp);
+		tq3 = temp - tq3;
+
+		if (q < 4)
+			return;
+		
+		System.out.println("tq1, tq2, tq3");
+		System.out.println(tq1 + "," + tq2 + "," + tq3);
+
+	}
+
+	public static void test() {
+
+		System.out.println("nbOfThreads, time");
+		String report;
+		int n = 2222222;
+
+		for (int i : threads) {
+
+			report = "";
+			long t0 = System.currentTimeMillis();
+
+			new UniquePointsFactory(i).randomize(n);
+
+			long tx = System.currentTimeMillis();
+
+			report += i + ",";
+			report += tx - t0;
+
+			System.out.println(report);
+		}
+
 	}
 
 	public static boolean validateArgs(int q, int n, int p) {
